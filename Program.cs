738addfa6c1512e045eaf5e2294a4b9293b98ced
AlexandrAlekseev <alexandrshuriken@gmail.com>
2022-08-07 22:29:07 +0300

    Выведу его для наглядности на консоль и в файл

--- Program.cs
+++ Program.cs
@@ -3,3 +3,6 @@ using static System.Console;
 Clear(); 
 
 string [] StartArray =  { "Hello", "2", "World", "12 34","1567","-2","e2" ,"Neapol","EnD"} ;
+
+WriteLine($"Массив заданый на старте выполнения: \n[{String.Join(",",StartArray)}]\n");
+File.WriteAllText("input.txt",($"Массив заданый на старте выполнения: \n[{String.Join(",",StartArray)}]\n"));
