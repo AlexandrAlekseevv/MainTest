ad5c6227cd620fff6df4573a272d840bdb821707
AlexandrAlekseev <alexandrshuriken@gmail.com>
2022-08-07 22:34:08 +0300

    описал метод

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string [] LessThanThree(string[] array)
+{
+    
+    int countLessThanThree = 0;
+    for(int j=0;j<array.Length;j++)
+    {
+        if(array[j].Length<=3)
+        {
+            countLessThanThree++;
+        }
+    }
+    string [] result = new string[countLessThanThree];
+    int indNewArray=0;
+    for(int i=0;i<array.Length;i++)
+    {
+        if(array[i].Length<=3)
+        {
+            result[indNewArray]=array[i];
+            indNewArray++;
+        }
+    }
+     return result;   
+}
